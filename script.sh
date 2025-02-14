e971d9ab8984cf085cfb61027d531fac2ea02cf6
İSmeet Sarı <54545086+jonasakid41@users.noreply.github.com>
2025-02-14 13:35:35 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,4 +0,0 @@
-    curl -o latest -L https://securedownloads.cpanel.net/latest
-    wget -O directadmin.sh https://www.directadmin.com/setup.sh
-    sudo su - -c "sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh)"
-    curl -fsSL https://installer.plesk.com/one-click-installer | sh
